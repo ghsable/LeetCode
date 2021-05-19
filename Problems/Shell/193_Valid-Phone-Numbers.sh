dc9b235653558cc0267d0a4a2439e2a643bf1d42
suna <sn.sable005@gmail.com>
2021-05-19 15:43:35 +0900

    Update 193_Valid-Phone-Numbers.sh

--- Problems/Shell/193_Valid-Phone-Numbers.sh
+++ Problems/Shell/193_Valid-Phone-Numbers.sh
@@ -10,14 +10,14 @@
 # 
 # Assume that file.txt has the following content:
 # 
-# 987-123-4567
-# 123 456 7890
-# (123) 456-7890
+#     987-123-4567
+#     123 456 7890
+#     (123) 456-7890
 # 
 # Your script should output the following valid phone numbers:
 # 
-# 987-123-4567
-# (123) 456-7890
+#     987-123-4567
+#     (123) 456-7890
 # 
 # Read from the file file.txt and output all valid phone numbers to stdout.
 
